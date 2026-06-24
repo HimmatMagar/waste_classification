56cf01d838efb775b8da77720f3fe2431a60941f
Himmat Magar <himmatmagar007@gmail.com>
2026-06-24 13:22:57 +0900

    updateing main.py file

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ from src.wasteClassification.pipeline.model_training_pipeline import ModelTraini
 from src.wasteClassification.pipeline.model_eval_pipeline import ModelEvalPipeline
 
 
-# STAGE_NAME = "Data Ingestion Stage"
-# try:
-#       logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
-#       obj = DataIngestionPipeline()
-#       obj.main()
-#       logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
-# except Exception as e:
-#       logger.exception(e)
-#       raise e
+STAGE_NAME = "Data Ingestion Stage"
+try:
+      logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+      obj = DataIngestionPipeline()
+      obj.main()
+      logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+      logger.exception(e)
+      raise e
 
 
-# stage_name = "Model Training Stage"
-# try:
-#       logger.info(f">>>>>> {stage_name} started <<<<<<")
-#       obj = ModelTrainingPipeline()
-#       obj.Train_Model()
-#       logger.info(f">>>>>> {stage_name} completed <<<<<<")
-# except Exception as e:
-#       logger.exception(e)
-#       raise e
+stage_name = "Model Training Stage"
+try:
+      logger.info(f">>>>>> {stage_name} started <<<<<<")
+      obj = ModelTrainingPipeline()
+      obj.Train_Model()
+      logger.info(f">>>>>> {stage_name} completed <<<<<<")
+except Exception as e:
+      logger.exception(e)
+      raise e
 
 STAGE_NAME = "Model Eval Stage"
 try:
